be980e9c93b5294fd9990b3060d6c6f6f8d801bc
Justin <justin.leluc@gmail.com>
2023-01-13 14:32:27 +0100

    depenses

--- app/src/Controllers/ExpenditureController.php
+++ app/src/Controllers/ExpenditureController.php
@@ -12,13 +12,15 @@ class ExpenditureController extends AbstractController
   #[Route('/create_expenditure', name: "create-expenditure", methods: ["POST", "GET"])]
   public function create_expenditure()
   {
-    // $id_creator = $_REQUEST['id_creator'];
+    $id_creator = $_REQUEST['id_creator'];
     $expenditureName = $_REQUEST['expenditureName'];
     $amount = $_REQUEST['amount'];
     $flat_share_id = $_REQUEST['flat_share_id'];
     $creation_date = $_REQUEST['creation_date'];
     $expenditureManager = new ExpenditureManager(new PDOFactory());
-
-    $expenditureManager->createExpenditure($expenditureName, $flat_share_id, $amount, $creation_date);
+    $countUser = $expenditureManager->countUser($flat_share_id);
+    $queryUser = $expenditureManager->userFlatShare($flat_share_id);
+    $amount = $amount / $countUser;
+    $expenditureManager->createExpenditure($expenditureName, $flat_share_id, $amount, $creation_date, $id_creator, $queryUser);
   }
 }
--- app/src/Managers/ExpenditureManager.php
+++ app/src/Managers/ExpenditureManager.php
@@ -8,21 +8,37 @@ use App\Interfaces\Database;
 
 class ExpenditureManager extends BaseManager
 {
-  public function createExpenditure(string $expenditureName, int $flat_share_id, int $amount, string $creation_date)
+  public function createExpenditure(string $expenditureName, int $flat_share_id, int $amount, string $creation_date, int $id_creator, $queryUser)
   {
-
-    $queryUser = $this->pdo->prepare('SELECT roommate_id FROM roomate_has_flat_share WHERE flat_share_id=:actual_flat_share_id');
-    $queryUser->bindValue('actual_flat_share_id', $flat_share_id, \PDO::PARAM_INT);
-    $queryUser->execute();
     while ($data = $queryUser->fetch(\PDO::FETCH_ASSOC)) {
+      if ($data['roommate_id'] == $id_creator) {
+        $payed = 1;
+      } else {
+        $payed = 0;
+      }
 
-      $query = $this->pdo->prepare('INSERT INTO expenditure ( expenditure_name ,flat_share_id , roommate_id, amount, creation_date) VALUES (:expenditureName, :flat_share_id, :roommate_id, :amount,:creation_date)');
+      $query = $this->pdo->prepare('INSERT INTO expenditure ( expenditure_name ,flat_share_id , roommate_id, amount, creation_date,payed) VALUES (:expenditureName, :flat_share_id, :roommate_id, :amount,:creation_date,:payed)');
       $query->bindValue('expenditureName', $expenditureName, \PDO::PARAM_STR);
       $query->bindValue('flat_share_id', $flat_share_id, \PDO::PARAM_INT);
       $query->bindValue('roommate_id', $data['roommate_id'], \PDO::PARAM_INT);
-      $query->bindValue('amount', $amount, \PDO::PARAM_INT);
+      $query->bindValue('amount', ($amount), \PDO::PARAM_INT);
       $query->bindValue('creation_date', $creation_date, \PDO::PARAM_STR);
+      $query->bindValue('payed', $payed, \PDO::PARAM_BOOL);
       $query->execute();
     }
+  }
+  public function countUser($flat_share_id)
+  {
+    $queryCountUser = $this->pdo->prepare('SELECT  roommate_id FROM roomate_has_flat_share WHERE flat_share_id=:actual_flat_share_id');
+    $queryCountUser->bindValue('actual_flat_share_id', $flat_share_id, \PDO::PARAM_INT);
+    $queryCountUser->execute();
+    return (count($queryCountUser->fetch()));
+  }
+  public function userFlatShare($flat_share_id)
+  {
+    $queryCountUser = $this->pdo->prepare('SELECT  roommate_id FROM roomate_has_flat_share WHERE flat_share_id=:actual_flat_share_id');
+    $queryCountUser->bindValue('actual_flat_share_id', $flat_share_id, \PDO::PARAM_INT);
+    $queryCountUser->execute();
+    return $queryCountUser;
   }
 }
