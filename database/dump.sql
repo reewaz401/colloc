fe219c8868c4a3d3a9665f6964ecf2d2074459b8
Nassim <113092073+NasssDev@users.noreply.github.com>
2023-01-13 09:43:56 +0100

    dump.sql updated

--- database/dump.sql
+++ database/dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : database
--- Généré le : mar. 10 jan. 2023 à 09:33
+-- Généré le : ven. 13 jan. 2023 à 08:39
 -- Version du serveur : 10.8.6-MariaDB-1:10.8.6+maria~ubu2204
 -- Version de PHP : 8.0.24
 
@@ -24,51 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `expenditure`
---
-
-CREATE TABLE `expenditure` (
-  `id` int(11) NOT NULL,
-  `roommate_id` int(11) NOT NULL,
-  `flat_share_id` int(11) NOT NULL,
-  `expenditure_name` varchar(255) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `creation_date` date NOT NULL DEFAULT current_timestamp(),
-  `payed` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `flat_share`
---
-
-CREATE TABLE `flat_share` (
-  `id` int(11) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `image` varchar(255) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `monthly_fee`
---
-
-CREATE TABLE `monthly_fee` (
-  `id` int(11) NOT NULL,
-  `flat_share_id` int(11) NOT NULL,
-  `fee_amount` int(255) NOT NULL,
-  `fee_name` varchar(255) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `roomate_has_flat_share`
 --
 
@@ -77,52 +32,14 @@ CREATE TABLE `roomate_has_flat_share` (
   `roommate_id` int(11) NOT NULL,
   `flat_share_id` int(11) NOT NULL,
   `role` tinyint(1) DEFAULT 0,
-  `rent_pro_rata` int(255) NULL,
+  `rent_pro_rata` int(255) DEFAULT NULL,
   `entry_date` date NOT NULL DEFAULT current_timestamp(),
-  `exit_date` date NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `roommate`
---
-
-CREATE TABLE `roommate` (
-  `id` int(11) NOT NULL,
-  `pwd` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `rol` varchar(255) NULL,
-  `date_naissance` date NOT NULL
+  `exit_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Index pour les tables déchargées
 --
-
---
--- Index pour la table `expenditure`
---
-ALTER TABLE `expenditure`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `flat_share_id` (`flat_share_id`),
-  ADD KEY `roommate_id` (`roommate_id`);
-
---
--- Index pour la table `flat_share`
---
-ALTER TABLE `flat_share`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `monthly_fee`
---
-ALTER TABLE `monthly_fee`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `flat_share_id` (`flat_share_id`);
 
 --
 -- Index pour la table `roomate_has_flat_share`
@@ -133,68 +50,25 @@ ALTER TABLE `roomate_has_flat_share`
   ADD KEY `flat_share_id` (`flat_share_id`);
 
 --
--- Index pour la table `roommate`
---
-ALTER TABLE `roommate`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
---
--- AUTO_INCREMENT pour la table `expenditure`
---
-ALTER TABLE `expenditure`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `flat_share`
---
-ALTER TABLE `flat_share`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `monthly_fee`
---
-ALTER TABLE `monthly_fee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `roomate_has_flat_share`
 --
 ALTER TABLE `roomate_has_flat_share`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `roommate`
---
-ALTER TABLE `roommate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `expenditure`
---
-ALTER TABLE `expenditure`
-  ADD CONSTRAINT `expenditure_ibfk_1` FOREIGN KEY (`flat_share_id`) REFERENCES `flat_share` (`id`),
-  ADD CONSTRAINT `expenditure_ibfk_2` FOREIGN KEY (`roommate_id`) REFERENCES `roommate` (`id`);
-
---
--- Contraintes pour la table `monthly_fee`
---
-ALTER TABLE `monthly_fee`
-  ADD CONSTRAINT `monthly_fee_ibfk_1` FOREIGN KEY (`flat_share_id`) REFERENCES `flat_share` (`id`);
-
---
 -- Contraintes pour la table `roomate_has_flat_share`
 --
 ALTER TABLE `roomate_has_flat_share`
-  ADD CONSTRAINT `roomate_has_flat_share_ibfk_1` FOREIGN KEY (`roommate_id`) REFERENCES `roommate` (`id`),
-  ADD CONSTRAINT `roomate_has_flat_share_ibfk_2` FOREIGN KEY (`flat_share_id`) REFERENCES `flat_share` (`id`);
+  ADD CONSTRAINT `roomate_has_flat_share_ibfk_1` FOREIGN KEY (`roommate_id`) REFERENCES `roommate` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `roomate_has_flat_share_ibfk_2` FOREIGN KEY (`flat_share_id`) REFERENCES `flat_share` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
