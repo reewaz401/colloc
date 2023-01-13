e83b7ba887940b5597504f884fe7f159e0ddabce
reewaz401 <reewazmaskey@gmail.com>
2023-01-13 14:40:58 +0100

    colloc_vie

---
+++ frontend/src/pages/colloc_view.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import  { useNavigate } from 'react-router-dom'
+
+export default function CollocView() {
+  const navigate = useNavigate();
+  return (
+    <>
+    
+      </>
+  );
+}
