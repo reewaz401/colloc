815ad297b4d09497af67b43f01fe1808dcfffb05
Justin <justin.leluc@gmail.com>
2023-01-15 22:19:38 +0100

    Creation&delete Month

---
+++ app/src/Traits/Notif.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Traits;
+use App\Entities\Expenditure;
+use App\Factories\PDOFactory;
+use App\Interfaces\Database;
+trait Notif
+{
+    public function notif($notif_date)
+    {
+      date_default_timezone_set('Europe/Berlin');
+      $notif_date=strtotime($notif_date);
+      if(time('d')<$notif_date){
+
+      }else{
+
+      }
+    }
+}
+$this->notif();
+?>
