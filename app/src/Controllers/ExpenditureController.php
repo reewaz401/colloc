d7aa7aa8305d44266145407fb45646a872781d2c
Justin <justin.leluc@gmail.com>
2023-01-19 20:16:24 +0100

    Add message

--- app/src/Controllers/ExpenditureController.php
+++ app/src/Controllers/ExpenditureController.php
@@ -24,11 +24,16 @@ class ExpenditureController extends AbstractController
     $expenditureManager = new ExpenditureManager(new PDOFactory());
     $countUser = $expenditureManager->countUser($flat_share_id);
     $queryUser = $expenditureManager->userFlatShare($flat_share_id);
-    var_dump($queryUser);die;
+    // var_dump($queryUser);die;
     $amount = $amount / $countUser;
     $amount =floatval(number_format($amount,2, '.',''));
     // var_dump ($amount);die;
-    $expenditureManager->createExpenditure($expenditureName, $flat_share_id, $amount, $id_creator, $queryUser);
+    $result=$expenditureManager->createExpenditure($expenditureName, $flat_share_id, $amount, $id_creator, $queryUser);
+       if ($result instanceof \Exception){
+            $this->renderJson('Erreur lors de la création de la dépense !', 555);
+            die;
+        }
+          $this->renderJson("La dépense a été créée avec succès !");
   }
   #[Route('/update_payed', name: "payed-expenditure", methods: ["POST", "GET"])]
   public function update_payed()
@@ -37,17 +42,29 @@ class ExpenditureController extends AbstractController
     $expenditureId = $_REQUEST['expenditureId'];
     $expenditureManageUpdate = new ExpenditureManager(new PDOFactory());
 
-    $expenditureManageUpdate->updatePayed($userId, $expenditureId);
+    $result=$expenditureManageUpdate->updatePayed($userId, $expenditureId);
+
+    if ($result instanceof \Exception){
+            $this->renderJson('Erreur lors du paiment de la dépense !', 555);
+            die;
+        }
+          $this->renderJson("La dépense a été payée avec succès !");
   }
   #[Route('/getUserExpenditure', name: "get-expenditure", methods: ["POST", "GET"])]
   public function getUserExpenditure()
   {
     $userId = $_REQUEST['user_id'];
     $flat_share_id = $_REQUEST['flat_share_id'];
-
+  $expenditureManageGet = new ExpenditureManager(new PDOFactory());
     // $getMonthFeeDate=$expenditureManageGet->UserExpenditure($flat_share_id);
     $data = $expenditureManageGet->UserExpenditure($userId, $flat_share_id);
+     if ($data instanceof \Exception){
+            $this->renderJson("Nous n'avons pas trouvé de dépenses pour cet utilisateur", 555);
+            die;
+        }
+          // $this->renderJson("Voici toutes vos dépenses");
     $this->renderJson($data);
+
   }
   #[Route('/deleteExpenditure', name: "delete-expenditure", methods: ["POST", "GET"])]
   public function deleteExpenditure()
@@ -56,7 +73,12 @@ class ExpenditureController extends AbstractController
     $expenditureId = $_REQUEST['expenditureId'];
     $expenditureManagerDelete = new ExpenditureManager(new PDOFactory());
 
-    $expenditureManagerDelete->deleteExpenditure($expenditureId);
+    $result=$expenditureManagerDelete->deleteExpenditure($expenditureId);
+     if ($result instanceof \Exception){
+            $this->renderJson('Erreur lors de la suppression de la dépense !', 555);
+            die;
+        }
+          $this->renderJson("La dépense a été supprimée avec succès !");
   }
   #[Route('/createMonthFee', name: "create-month", methods: ["POST", "GET"])]
   public function createMonthFee()
@@ -67,7 +89,12 @@ class ExpenditureController extends AbstractController
     $date = $_REQUEST['date'];
     $expenditureManagerCreate = new ExpenditureManager(new PDOFactory());
 
-    $expenditureManagerCreate->createMonthFee($fee_name,$flat_share_id,$feeAmount,$date);
+    $result= $expenditureManagerCreate->createMonthFee($fee_name,$flat_share_id,$feeAmount,$date);
+       if ($result instanceof \Exception){
+            $this->renderJson('Erreur lors de la création de la dépense mensuel !', 555);
+            die;
+        }
+          $this->renderJson("La dépense mensuel a été créée avec succès !");
   }
   #[Route('/delteMonthFee', name: "create-month", methods: ["POST", "GET"])]
   public function deleteMonthFee()
@@ -76,7 +103,12 @@ class ExpenditureController extends AbstractController
 
     $expenditureManagerDelete = new ExpenditureManager(new PDOFactory());
 
-    $expenditureManagerDelete->deleteMonthFee($monthFeedId);
+    $result= $expenditureManagerDelete->deleteMonthFee($monthFeedId);
+       if ($result instanceof \Exception){
+            $this->renderJson('Erreur lors de la suppression de la dépense mensuel !', 555);
+            die;
+        }
+          $this->renderJson("La dépense mensuel a été supprimée avec succès !");
   }
   #[Route('/getMonthFee', name: "get-month", methods: ["POST", "GET"])]
   public function getMonthFee()
@@ -85,6 +117,11 @@ class ExpenditureController extends AbstractController
 
     $expenditureManagerGetMonth = new ExpenditureManager(new PDOFactory());
 
-    $expenditureManagerGetMonth->getMonthFee($flat_share_id);
+    $result=$expenditureManagerGetMonth->getMonthFee($flat_share_id);
+       if ($result instanceof \Exception){
+            $this->renderJson("Nous n'avons pas trouvé la dépense mensuel !", 555);
+            die;
+        }
+          $this->renderJson($result);
   }
 }
--- app/src/Managers/ExpenditureManager.php
+++ app/src/Managers/ExpenditureManager.php
@@ -5,6 +5,7 @@ namespace App\Managers;
 use App\Entities\Expenditure;
 use App\Factories\PDOFactory;
 use App\Interfaces\Database;
+use PDOException;
 
 class ExpenditureManager extends BaseManager
 {
@@ -17,8 +18,8 @@ class ExpenditureManager extends BaseManager
       } else {
         $payed = 0;
       }
-
-      $query = $this->pdo->prepare('INSERT INTO expenditure ( expenditure_name ,flat_share_id , roommate_id, amount,payed,uniqId) VALUES (:expenditureName, :flat_share_id, :roommate_id, :amount,:payed,:uniqId)');
+      try{
+ $query = $this->pdo->prepare('INSERT INTO expenditure ( expenditure_name ,flat_share_id , roommate_id, amount,payed,uniqId) VALUES (:expenditureName, :flat_share_id, :roommate_id, :amount,:payed,:uniqId)');
       $query->bindValue('expenditureName', $expenditureName, \PDO::PARAM_STR);
       $query->bindValue('flat_share_id', $flat_share_id, \PDO::PARAM_INT);
       $query->bindValue('roommate_id', $data['roommate_id'], \PDO::PARAM_INT);
@@ -26,15 +27,22 @@ class ExpenditureManager extends BaseManager
       $query->bindValue('payed', $payed, \PDO::PARAM_BOOL);
       $query->bindValue('uniqId', ($uniqId), \PDO::PARAM_STR);
       $query->execute();
+      }catch  (\Exception $e){
+        return $e;
+      }
+
     }
   }
   public function countUser($flat_share_id)
   {
-    $queryCountUser = $this->pdo->prepare('SELECT COUNT(*)  FROM roomate_has_flat_share WHERE flat_share_id=:actual_flat_share_id');
+
+$queryCountUser = $this->pdo->prepare('SELECT COUNT(*)  FROM roomate_has_flat_share WHERE flat_share_id=:actual_flat_share_id');
     $queryCountUser->bindValue('actual_flat_share_id', $flat_share_id, \PDO::PARAM_INT);
     $queryCountUser->execute();
     $data = $queryCountUser->fetch();
     return $data[0];
+
+
   }
   public function userFlatShare($flat_share_id)
   {
@@ -45,23 +53,34 @@ class ExpenditureManager extends BaseManager
   }
   public function updatePayed(int $userId, int $expenditureId)
   {
+     try{
     $queryPayed = $this->pdo->prepare('UPDATE expenditure SET payed="1" WHERE id =:expenditureId AND roommate_id =:userId');
     $queryPayed->bindValue('expenditureId', $expenditureId, \PDO::PARAM_INT);
     $queryPayed->bindValue('userId', $userId, \PDO::PARAM_INT);
     $queryPayed->execute();
+     }catch  (\Exception $e){
+        var_dump ($e-getMessage());die;
+        return $e;
+      }
     // return $queryUser;
   }
   public function UserExpenditure(int $userId, int $flat_share_id)
   {
+     try{
     $queryUserExpenditure = $this->pdo->prepare('SELECT * FROM expenditure WHERE flat_share_id =:flatShareId AND roommate_id =:userId');
     $queryUserExpenditure->bindValue('flatShareId', $flat_share_id, \PDO::PARAM_INT);
     $queryUserExpenditure->bindValue('userId', $userId, \PDO::PARAM_INT);
     $queryUserExpenditure->execute();
     $data = $queryUserExpenditure->fetchAll(\PDO::FETCH_ASSOC);
     return $data;
+     }catch  (\Exception $e){
+
+        return $e;
+      }
   }
   public function deleteExpenditure(int $expenditureId)
   {
+      try{
     $queryFindAllExpenditure = $this->pdo->prepare('SELECT uniqId FROM expenditure WHERE id =:expenditureId ');
     $queryFindAllExpenditure->bindValue('expenditureId', $expenditureId, \PDO::PARAM_INT);
     $queryFindAllExpenditure->execute();
@@ -71,37 +90,51 @@ class ExpenditureManager extends BaseManager
     $querydeleteExpenditure = $this->pdo->prepare('DELETE FROM expenditure WHERE uniqId =:uniqId ');
     $querydeleteExpenditure->bindValue('uniqId', $data['uniqId'], \PDO::PARAM_STR);
     $querydeleteExpenditure->execute();
+     }catch  (\PDOException $e){
+      var_dump ($e-getMessage());die;
+        return $e;
+      }
   }
   public function createMonthFee(string $feeName, int $flat_share_id, int $feeAmount, int $date)
   {
 
-
+  try{
       $query = $this->pdo->prepare('INSERT INTO monthly_fee ( flat_share_id ,fee_amount,fee_name,date) VALUES (:flat_share_id, :fee_amount, :fee_name, :date)');
       $query->bindValue('fee_name', $feeName, \PDO::PARAM_STR);
       $query->bindValue('flat_share_id', $flat_share_id, \PDO::PARAM_INT);
       $query->bindValue('fee_amount', ($feeAmount), \PDO::PARAM_INT);
       $query->bindValue('date', $date, \PDO::PARAM_INT);
       $query->execute();
+       }catch  (\Exception $e){
+        return $e;
+      }
 
   }
   public function deleteMonthFee(int $monthFeeId)
   {
 
-
+  try{
       $querydeleteMonthFee = $this->pdo->prepare('DELETE  FROM monthly_fee WHERE id =:id ');
       $querydeleteMonthFee->bindValue('id', $monthFeeId, \PDO::PARAM_INT);
 
       $querydeleteMonthFee->execute();
+       }catch  (\Exception $e){
+        return $e;
+      }
 
   }
   public function getMonthFee(int $flat_share_id)
   {
+      try{
     $query = $this->pdo->prepare('SELECT * FROM monthly_fee WHERE flat_share_id =:flat_share_id ');
     $query->bindValue('flat_share_id', $flat_share_id, \PDO::PARAM_INT);
     $query->execute();
     $data = $query->fetch(\PDO::FETCH_ASSOC);
 
     return $data;
+     }catch  (\Exception $e){
+        return $e;
+      }
 
   }
   // public function createExpenditureMonth(string $expenditureName, int $flat_share_id, int $amount, string $creation_date, int $id_creator, $queryUser)
